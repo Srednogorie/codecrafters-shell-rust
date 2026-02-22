5be5d5103903afb09b9996bfdeef90dd366b2446
Alexander Krachunov <akrachunov@gmail.com>
2026-02-22 19:43:33 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::os::unix::fs::PermissionsExt;
+
 
 fn take_input(input: &mut String) {
     input.clear();
@@ -12,13 +14,37 @@ fn command_echo(args: &[&str]) {
     println!("{}", args.join(" "));
 }
 
+fn check_unknown_command(command: &str) {
+    let key = "PATH";
+    let mut found = false;
+    match std::env::var_os(key) {
+        Some(paths) => {
+            for path in std::env::split_paths(&paths) {
+                let path = format!("{}/{}", path.to_str().unwrap(), command);
+                if std::fs::metadata(&path).is_ok() {
+                    // Check if the file is executable
+                    if std::fs::metadata(&path).unwrap().permissions().mode() & 0o111 != 0 {
+                        found = true;
+                        println!("{} is {}", command, &path);
+                        break;
+                    }
+                }
+            }
+        }
+        None => {}
+    }
+    if !found {
+        println!("{}: not found", command);
+    }
+}
+
 fn command_type(args: &[&str]) {
     let command = args.first().unwrap_or(&"");
     match *command {
         "echo" => println!("echo is a shell builtin"),
         "type" => println!("type is a shell builtin"),
         "exit" => println!("exit is a shell builtin"),
-        _ => println!("{}: not found", command),
+        _ => check_unknown_command(command),
     }
 }
 
