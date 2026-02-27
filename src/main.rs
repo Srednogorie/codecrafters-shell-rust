1b911cdf87c6edd5c91b70f4129fa8d23740c1b5
Alexander Krachunov <akrachunov@gmail.com>
2026-02-27 23:36:44 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -14,23 +14,57 @@ fn take_input(input: &mut String) {
     io::stdin().read_line(input).unwrap();
 }
 
+fn parse_input(input: &str) -> Vec<String> {
+    let input = input.replace("''", "");
+    // read_line` includes the **newline character** `'\n'` at the end of the input
+    // trim it or it's going to sneak into the command
+    let input = input.trim();
+    
+    let mut tokens = Vec::new();
+    
+    let mut token = String::new();
+    let mut inside_single_quote = false;
+    for char in input.chars() {
+        match char {
+            ' ' => {
+                if inside_single_quote {
+                    token.push(char);
+                } else {
+                    if !token.is_empty() {
+                        tokens.push(std::mem::take(&mut token))
+                    }
+                }
+            }
+            '\'' => {
+                if inside_single_quote {
+                    inside_single_quote = false;
+                } else {
+                    inside_single_quote = true;
+                }
+            }
+            _ => {
+                token.push(char);
+            }
+        }
+    }
+    
+    if !token.is_empty() {
+        tokens.push(std::mem::take(&mut token));
+    }
+    tokens
+}
+
 fn main() {
     let mut input = String::new();
     loop {
         take_input(&mut input);
-        let mut iter = input.split_whitespace();
+        let iter = parse_input(&input);
         
-        let command = match iter.next() {
-            Some(cmd) => cmd,
-            None => continue,  // empty input, no need for the is_empty check
-        };
-
-        let args: Vec<String> = iter.map(|s| s.to_string()).collect();
-
-        match Commands::from_str(command, &args) {
+        let (command, args) = iter.split_first().unwrap();
+        
+        match Commands::from_str(command, args) {
             Some(cmd) => cmd.execute(),
-            // None => println!("{}: command not found", input.trim()),
-            None => check_unknown_command(command, args, true),
+            None => check_unknown_command(command, args.to_vec(), true),
         }
     }
 }
